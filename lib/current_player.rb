4e09d7b9bdda7dfb26b67ae3fd0a10d7acca20f1
Thomas Winter <wintert98@gmail.com>
2020-10-04 17:39:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,7 @@ def turn_count(board1)
 end
 
 def current_player(player)
-  if turn_count(board) % 2 == 0 "X" : "O"
+  if turn_count(board) % 2 == 0 "X" : 
   
    else turn_count(board) % 2 == 1  
 end
