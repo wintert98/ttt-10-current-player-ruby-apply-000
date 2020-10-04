7261e6bc2106bab62ad1b26a9cb5367fad6c0d7a
Thomas Winter <wintert98@gmail.com>
2020-10-04 17:56:45 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+    counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+    end
+    counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  
+   else turn_count(board) % 2 == 1 
+     return "O"
+   end
+end
