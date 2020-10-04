11aee9dd6366b8db66eec7deecb8af03086ee434
Thomas Winter <wintert98@gmail.com>
2020-10-04 17:35:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,11 @@
 def turn_count(board1)
-  
+    counter = 0
+   board.each do |spaces|
+      if spaces == "X" || spaces == "O"
+         counter += 1
+      end
+   end
+   counter
 end
 
 def current_player(player)
